104e706e814a19a32999a64c75dc9a7a23945c73
gustavcampos <gustavop.campos2004@gmail.com>
2024-08-11 17:56:57 -0300

    Project Base Done!

--- js/lang.js
+++ js/lang.js
@@ -4,8 +4,10 @@ function changeLanguage(langCode) {
 	for (let tag of langTags) {
 		if (tag.getAttribute("data-lang") === langCode) {
 			tag.classList.remove("is-hidden");
+			tag.setAttribute("aria-hidden", "false");
 		} else {
 			tag.classList.add("is-hidden");
+			tag.setAttribute("aria-hidden", "true");
 		}
 	}
 
@@ -13,17 +15,19 @@ function changeLanguage(langCode) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+	const buttonClass = ["is-primary", "is-selected"]
+
 	changeLanguage("en");
 
 	const langButtons = document.querySelectorAll(".lang-button");
 
 	for (let button of langButtons) {
 		button.addEventListener("click", () => {
-			for (let button of langButtons) {button.classList.remove("is-info", "is-selected");}
+			for (let button of langButtons) {button.classList.remove(...buttonClass);}
 			
 			changeLanguage(button.value);
 
-			button.classList.add("is-info", "is-selected");
+			button.classList.add(...buttonClass);
 		});
 	}
 });
--- js/theme.js
+++ js/theme.js
@@ -1,53 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-	// Get all dropdowns
-	const $dropdowns = document.querySelectorAll(".dropdown");
+function getBrowserTheme() {
+	if (window.matchMedia && 
+		window.matchMedia('(prefers-color-scheme: dark)').matches
+	) {return "dark";}
 
+	return "light";
+}
+
+function changeTheme(theme, mainThemeButton) {
+	const evalTheme = ["dark", "light"].includes(theme) ? theme : getBrowserTheme();
+	const buttonClass = evalTheme === "dark" ? "primary" : "warning";
+	const imgs = mainThemeButton.querySelectorAll("img");
+	// Scaling up to whole dropdown element
+	const dropdownElement = mainThemeButton.parentElement.parentElement;
+	
+	document.documentElement.setAttribute("data-theme", evalTheme);
+
+	imgs.forEach(img => img.classList.add("is-hidden"));
+
+	mainThemeButton.querySelector(`.${buttonClass}`).classList.remove('is-hidden');
+
+	dropdownElement.classList.remove("is-active");
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
 	// Get all "theme-button" elements
 	const $themeButtons = document.querySelectorAll('button.theme-button');
 	const $mainThemeButton = document.getElementById("theme-button");
 
-	for (let dropdown of $dropdowns) {
-		const mainButton = dropdown.querySelector(".dropdown-trigger button");
-
-		console.log(mainButton);
-	
-		mainButton.addEventListener("click", () => {
-			dropdown.classList.toggle("is-active");	
-		});
-	}
-
-	// Close the dropdown if clicked outside
-    document.addEventListener('click', (event) => {
-    	for (let dropdown of $dropdowns) {
-    		if (!dropdown.contains(event.target)) {
-	    		dropdown.classList.remove("is-active");
-    		}
-    	}
-    });
+	changeTheme(null, $mainThemeButton);
 
 	// Add a click event on each of them
 	for (let button of $themeButtons) {	
 		button.addEventListener('click', () => {
-			// Get the target from the "data-target" attribute
-			const htmlElement = document.documentElement;
-			const currentThemeImgs = $mainThemeButton.querySelectorAll("img");
-			const dataSchema = button.getAttribute("data-schema");
-
-			htmlElement.setAttribute("data-theme", dataSchema)
-
-			const classType = {
-				light: "warning",
-				dark: "link",
-				system: "primary",
-			}
-
-			for (let image of currentThemeImgs) {
-				if (image.classList.contains(`fill-${classType[dataSchema]}`)) {
-					image.classList.remove("is-hidden");
-				} else {
-					image.classList.add("is-hidden");
-				}
-			}
+			changeTheme(button.getAttribute("data-schema"), $mainThemeButton);
 		});
 	};
 
